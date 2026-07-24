368f4bdb074cc9b3b179d0709821bd3d788e4b2a
Kekere Oshioke Divine <136168388+Oshiokex@users.noreply.github.com>
2026-07-24 19:23:33 +0100

    Update main.py

--- Python Project/currencyConverter/main.py
+++ Python Project/currencyConverter/main.py
@@ -1,16 +1,16 @@
 import converter
 
 currencies = {
-    "euros" : {"rate" : 1250},
-    "dollars" : {"rate" : 1250},
-    "rupies" : {"rate" : 1250},
+    "euros" : {"rate" : 1557.10},
+    "dollars" : {"rate" : 1366.81},
+    "rupees" : {"rate" : 14.18},
 }
 
-print("---> Welcome to my currency converter <---")
+print("---> Welcome to my currency converter (From Naira To ...) <---")
 
 while True:
     Choice = input("""
-Enter the Currency you want to convert to 
+From Naira to what currency: 
 -> Dollars
 -> Euros
 -> Rupies
