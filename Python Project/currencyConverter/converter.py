2133e0bcd6160ecaff6494d5c67662539c2941f7
Kekere Oshioke Divine <136168388+Oshiokex@users.noreply.github.com>
2026-07-24 19:21:11 +0100

    Update converter.py

--- Python Project/currencyConverter/converter.py
+++ Python Project/currencyConverter/converter.py
@@ -1,14 +1,14 @@
 currencies = {
-    "euros" : {"rate" : 1250},
-    "dollars" : {"rate" : 1250},
-    "rupies" : {"rate" : 1250},
+    "euros" : {"rate" : 1557.10},
+    "dollars" : {"rate" : 1366.81},
+    "rupees" : {"rate" : 14.18},
 }
 
 def convert(Choice):
     global currencies
     if Choice in currencies:    
         rate = currencies[Choice]["rate"]
-        amt_naira = int(input("Enter amount in naira> NGN: "))
+        amt_naira = float(input("Enter amount in naira> NGN: "))
         newamt = amt_naira / rate
         print(f"{amt_naira} Naira(s) is equal to {newamt} {Choice}")
 
